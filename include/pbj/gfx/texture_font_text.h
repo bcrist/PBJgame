de415261154e3ecda7b6ca950863fb1b30c6351c
Ben Crist <ben@magicmoremagic.com>
2013-07-21 21:05:23 -0500

    ShaderProgram, started /TextureFont(Text)?/

---
+++ include/pbj/gfx/texture_font_text.h
@@ -0,0 +1,70 @@
+// Copyright (c) 2013 PBJ^2 Productions
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+///////////////////////////////////////////////////////////////////////////////
+/// \file   pbj/gfx/texture_font_text.h
+/// \author Benjamin Crist
+///
+/// \brief  pbj::gfx::TextureFontText class header.
+
+#ifndef PBJ_GFX_TEXTURE_FONT_TEXT_H_
+#define PBJ_GFX_TEXTURE_FONT_TEXT_H_
+
+#include "pbj/gfx/texture_font.h"
+
+namespace pbj {
+namespace gfx {
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief  Represents a 
+class TextureFontText
+{
+public:
+    TextureFontText(const TextureFont& font, const std::string& text);
+    ~TextureFontText();
+
+    void setColor(const vec4& color);
+    const vec4& getColor() const;
+
+    void draw(const mat4& transform);
+
+
+
+private:
+    vec4 color_;
+   
+    be::ConstHandle<Texture> texture_;
+
+    GLint color_uniform_location_;
+    GLint texture_uniform_location_;
+    GLint transform_uniform_location_;
+
+    GLuint vao_id_; ///< OpenGL Vertex array object id
+    GLuint ibo_id_; ///< OpenGL Vertex Index buffer object id
+    GLuint vbo_id_; ///< OpenGL Vertex buffer object id
+
+    TextureFontText(const TextureFontText&);
+    void operator=(const TextureFontText&);
+};
+
+} // namespace pbj::gfx
+} // namespace pbj
+
+#endif
--- src/pbj/gfx/texture_font.cpp
+++ src/pbj/gfx/texture_font.cpp
@@ -19,57 +19,20 @@
 // IN THE SOFTWARE.
 
 ///////////////////////////////////////////////////////////////////////////////
-/// \file   pbj/gfx/shader_program.h
+/// \file   pbj/gfx/texture_font.cpp
 /// \author Benjamin Crist
 ///
-/// \brief  pbj::gfx::ShaderProgram class header.
+/// \brief  Implementations of pbj::gfx::TextureFont functions.
 
-#ifndef PBJ_GFX_SHADER_PROGRAM_H_
-#define PBJ_GFX_SHADER_PROGRAM_H_
-
-#include "pbj/gfx/shader.h"
+#include "pbj/gfx/texture_font.h"
 
 namespace pbj {
 namespace gfx {
 
-///////////////////////////////////////////////////////////////////////////////
-/// \brief  Represents a program consisting of one or more shaders.
-///
-/// \details Usually a single vertex shader and a single fragment shader are
-///         used together.
-///
-/// \sa     Shader
-class ShaderProgram
+TextureFont::~TextureFont()
 {
-public:
-    ShaderProgram(const sw::ResourceId& id, const Shader& vertex_shader, const Shader& fragment_shader);
-    template <typename Iterator>
-    ShaderProgram(const sw::ResourceId& id, const Iterator& begin, const Iterator& end);
-    ~ShaderProgram();
+}
 
-    const be::Handle<ShaderProgram>& getHandle();
-    const be::ConstHandle<ShaderProgram>& getHandle() const;
-
-    const sw::ResourceId& getId() const;
-
-    GLuint getGlId() const;
-
-private:
-    void checkLinkResult_();
-    void invalidate_();
-
-    be::SourceHandle<ShaderProgram> handle_;
-    sw::ResourceId resource_id_;
-
-    GLuint gl_id_;
-
-    ShaderProgram(const ShaderProgram&);
-    void operator=(const ShaderProgram&);
-};
 
 } // namespace pbj::gfx
 } // namespace pbj
-
-#include "pbj/gfx/shader_program.inl"
-
-#endif
