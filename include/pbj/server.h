4420817a19bac76759bc4a809f8383022531860a
Peter Bartosch <bartoschp@gmail.com>
2013-08-05 21:13:55 -0500

    Commenting outline

--- include/pbj/server.h
+++ include/pbj/server.h
@@ -1,3 +1,9 @@
+////////////////////////////////////////////////////////////////////////////////
+/// \file	C:\Users\pbartosch_sa\Documents\Visual Studio 2012\Projects\
+/// 		PBJgame\include\pbj\server.h
+///
+/// \brief	Declares the server class.
+////////////////////////////////////////////////////////////////////////////////
 #ifndef SERVER_H_
 #define SERVER_H_
 
@@ -14,9 +20,18 @@ using pbj::net::Transport;
 
 namespace pbj
 {
+	////////////////////////////////////////////////////////////////////////////
+	/// \class	Server
+	///
+	/// \brief	Server.
+	///
+	/// \author	Peter Bartosch
+	/// \date	2013-08-05
+	////////////////////////////////////////////////////////////////////////////
 	class Server
 	{
 	public:
+		/// \brief	Maximum size of a packet.
 		static const I32 maxPacketSize = 512;
 
 		static bool init(I32, U32);
--- src/pbj/server.cpp
+++ src/pbj/server.cpp
@@ -1,11 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////
+/// \file	C:\Users\pbartosch_sa\Documents\Visual Studio 2012\Projects\
+/// 		PBJgame\src\pbj\server.cpp
+///
+/// \brief	Implements the server class.
+////////////////////////////////////////////////////////////////////////////////
 #ifndef SERVER_H_
 #include "pbj/server.h"
 #endif
 
 using namespace pbj;
 
+/// \brief	The server instance.
 Server* Server::_instance = 0;
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn	bool Server::init(I32 maxClients, U32 fps)
+///
+/// \brief	Initialises this object.
+///
+/// \author	Peter Bartosch
+/// \date	2013-08-05
+///
+/// \param	maxClients	The maximum clients.
+/// \param	fps		  	The FPS.
+///
+/// \return	true if it succeeds, false if it fails.
+////////////////////////////////////////////////////////////////////////////////
 bool Server::init(I32 maxClients, U32 fps)
 {
 	assert(!_instance);
@@ -40,6 +60,7 @@ bool Server::init(I32 maxClients, U32 fps)
 						<< "Timeout:\t"<< cfg.timeout << PBJ_LOG_NL
 						<< "Max Nodes:\t"<< cfg.maxNodes << PBJ_LOG_END;
 
+	///< An enum constant representing the instance > dt option
 	_instance->_dt = 1.0f/fps;
 	U8 hostname[64+1] = "hostname";
 	_instance->_transport->getHostName(hostname, sizeof(hostname));
@@ -50,6 +71,16 @@ bool Server::init(I32 maxClients, U32 fps)
    return true;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn	I32 Server::run()
+///
+/// \brief	Gets the run.
+///
+/// \author	Peter Bartosch
+/// \date	2013-08-05
+///
+/// \return	.
+////////////////////////////////////////////////////////////////////////////////
 I32 Server::run()
 {
 	do
@@ -60,6 +91,14 @@ I32 Server::run()
 	return 0;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn	void Server::stop()
+///
+/// \brief	Stops this object.
+///
+/// \author	Peter Bartosch
+/// \date	2013-08-05
+////////////////////////////////////////////////////////////////////////////////
 void Server::stop()
 {
 	if(_instance != 0)
@@ -67,6 +106,7 @@ void Server::stop()
 		if(_instance->_transport != 0)
 		{
 			Transport::destroy(_instance->_transport);
+			///< An enum constant representing the instance > transport option
 			_instance->_transport = nullptr;
 		}
 		delete _instance;
@@ -75,15 +115,41 @@ void Server::stop()
 	Transport::shutdown();
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn	Server::Server()
+///
+/// \brief	Default constructor.
+///
+/// \author	Peter Bartosch
+/// \date	2013-08-05
+////////////////////////////////////////////////////////////////////////////////
 Server::Server() :
 	_transport(nullptr),
 	_dt(0.0f),
 	_sendMsgTimer(0.0f)
 {}
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn	Server::~Server()
+///
+/// \brief	Destructor.
+///
+/// \author	Peter Bartosch
+/// \date	2013-08-05
+////////////////////////////////////////////////////////////////////////////////
 Server::~Server()
 {}
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn	bool Server::update()
+///
+/// \brief	Updates this object.
+///
+/// \author	Peter Bartosch
+/// \date	2013-08-05
+///
+/// \return	true if it succeeds, false if it fails.
+////////////////////////////////////////////////////////////////////////////////
 bool Server::update()
 {
 	_transport->update(_dt);
